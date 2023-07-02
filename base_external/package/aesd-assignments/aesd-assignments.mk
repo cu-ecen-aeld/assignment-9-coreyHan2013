e16ef86198de8fa970ecbf9b8579bdafc1b6d1e2
coreyHan2013 <gchhan@gmail.com>
2023-07-02 15:55:01 -0400

    assignment4p2: modified commit number of assignment3 url

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = cef3eb912f94c8dcb45cfa5905fb47daedf44f57
+AESD_ASSIGNMENTS_VERSION = 5969224aab7ba8d29aea34992a6f2cf89f73f392
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
